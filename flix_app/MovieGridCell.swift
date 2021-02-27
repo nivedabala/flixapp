e8631126f4a5a6ebbd3a4365030e88ae454fb054
Niveda <niveda4@hotmail.com>
2021-02-27 00:10:34 -0500

    updates

---
+++ flix_app/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flix_app
+//
+//  Created by Niveda Balananthan on 2021-02-26.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
